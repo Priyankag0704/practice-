938791388463ba68d31b8f503829aa1e3088d362
Priyankag0704 <126230404+Priyankag0704@users.noreply.github.com>
2023-03-19 20:52:43 +0530

    Add files via upload

---
+++ Props/Nav1.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+const Nav1 =(props)=>{
+    return(
+        // <h1>{props.girl}</h1>
+        // <h1>{props.data.username}</h1>
+        // <img src={props.data} alt="pic" />
+        <>
+        {/* <h1>{props.data}{props.num}</h1> */}
+        <h1>{props.boolean}{props.wrd}</h1>
+        </>
+    )
+}
+export default Nav1;
